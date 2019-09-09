d31841f32aada6d18053e8be71f4b22b429f9e9b
nightism <sun.mingyang@gmx.com>
2019-09-09 12:04:05 +0800

    update constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const EMPTY = {
+    LIST: [],
+    DICT: {},
+    STRING: '',
+    NULL: null,
+    UNDEFINED: undefined,
+}
+
+export const BOOL = {
+    TRUE: true,
+    FALSE: false,
+}
+
+export const DEFAULT_TABLE_PAGE_SIZE = 10
